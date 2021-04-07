5b0a1fa4d1657b352965b6ce58b2d751207f6e7e
Luciano dii Souza <lucianodiisouza@hotmail.com>
2021-04-06 21:22:00 -0300

    done: auth screen layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,57 @@
-import Head from "next/head";
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 
 export default function Home() {
-  return <h1>Dash</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="messenger.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgcolor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="senha">Senha</FormLabel>
+            <Input
+              name="password"
+              id="senha"
+              type="password"
+              focusBorderColor="messenger.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgcolor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="messenger">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
